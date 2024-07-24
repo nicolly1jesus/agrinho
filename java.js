326b03ae9ba1fa8f75f68de5562a60c8c222e21f
nicolly <135882135+nicolly1jesus@users.noreply.github.com>
2024-07-24 08:04:38 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+//Primeiro
+function visita() {
+    const width = 800;
+    const height = 600;
+    const left = (screen.widht/2) - (width/2);
+    const top = (screen.height/2) - (height/2);
+    window.open('visita.html','pagamento','width=${width},height=${height},top=${top},left=${left}');}
